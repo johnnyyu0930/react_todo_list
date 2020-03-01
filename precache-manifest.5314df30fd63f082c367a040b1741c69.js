1d052ae106ae309cbc2567f5d8063fbc8903dca4
johnnyyu0930 <johnnyyu@awoo.com.tw>
2020-03-01 22:54:41 +0800

    Updates

---
+++ precache-manifest.5314df30fd63f082c367a040b1741c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad29cee2d2289afaf851da490894f8e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11eaca41e5ceb61560ae",
+    "url": "/static/css/main.d4f73466.chunk.css"
+  },
+  {
+    "revision": "8ec76ecd65d7803d2cec",
+    "url": "/static/js/2.4956b064.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4956b064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11eaca41e5ceb61560ae",
+    "url": "/static/js/main.265242e0.chunk.js"
+  },
+  {
+    "revision": "22bc8f2f985a366d0031",
+    "url": "/static/js/runtime-main.3673504e.js"
+  }
+]);
